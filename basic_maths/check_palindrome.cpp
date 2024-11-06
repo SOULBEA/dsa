4052ff94ca9cb09a6fc5791ad4b469aa6799eab2
snorlax_codes <siarpit119@gmail.com>
2024-11-06 18:01:02 +0530

    basic_math

---
+++ basic_maths/check_palindrome.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"please enter a number; ";
+  cin>>n;
+  int dup = n;
+  int rev = 0;
+  while(n>0){
+    int last_digit = n%10;
+    rev = (rev * 10) + last_digit;
+    n = n/10;
+  }
+  if(dup == rev){
+    cout<<"true";
+  }
+  else cout<<"false";
+  return 0;
+}
--- basic_maths/reverse_number.cpp
+++ basic_maths/reverse_number.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
   int n;
   cout<<"please enter a number: ";
   cin>>n;
-  int count = 0;
+  int rev = 0;
   while(n>0){
     int last_digit = n%10;
-    count = count+1;
+    rev = (rev * 10)+last_digit;
     n = n/10;
   }
-  cout<<count<<endl;
+  cout<<rev<<endl;
   return 0;
 }
