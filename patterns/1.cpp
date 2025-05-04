5323ea4df4e8f1972d305fb1b867f3ba38b69d2f
snorlax_codes <siarpit119@gmail.com>
2025-05-04 18:25:06 +0530

    learning patterns

--- patterns/1.cpp
+++ patterns/1.cpp
@@ -85,10 +85,46 @@ void pattern8(int n){
   }
 }
 
+void pattern9(int n){
+  for(int i = 0; i<n; i++){
+    for(int j = 0; j<n-i+1; j++){
+      cout<<" ";
+    }
+    for(int j = 0; j<2*i+1; j++){
+      cout<<"*";
+    }
+    for(int j = 0; j<n-i+1; j++){
+      cout<<" ";
+    }
+    for(int j = 0; j<i;j++){
+      cout<<" ";
+    }
+    for(int j = 0; j<2*n - (2*i+1); j++){
+      cout<<"*";
+    }
+    for(int j = 0; j<i; j++){
+      cout<<" ";
+    }
+    cout<<endl;
+  }
+  cout<<endl;
+}
+
+void pattern10(int n){
+  for(int i = 1; i<=2*n-1; i++){
+    int stars = i;
+    if(i > n) stars = 2*n-1;
+    for(int j = 1; j<=stars;j++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+}
+
 int main(){
   int n;
   cout<<"enter number: ";
   cin>>n;
-  pattern8(n);
+  pattern10(n);
   return 0;
 }
